25922329863ff6bb329370150972d019d9799d5a
liufei <liufei@zhuanzhuan.com>
2022-09-01 23:56:29 +0800

    20220901

--- learn/list.py
+++ learn/list.py
@@ -1,9 +1,30 @@
-a=[1,'s',False,100]
+a=[1,'s',False,100,5,5,7,9,False]
 b=[2]
 print(a+b)
 # 字符串不一样的是，列表中的元素是可以改变的
-a[1:] = []
+# a[2:] = []
+a.insert(1,"第一个")
+# 删除对象不存在就会报错
+a.remove('第一个')
+print(a.pop(1))
+# a.sort(reverse=True)
+print('1的次数',a.count(1))
+a[a.index(100)] = 101
+# index返回第一个查到的下标 index(start,end)
+print('a===',a)
+print('False下标',a.index(False))
+print('False2下标',a.index(False,a.index(False)+1))
+print('浅拷贝',a.copy(),a[:])
 print(a)
+# print(a[:0])
+# print(a[::2])
+# print(a[0:3:3])
+# 只能添加一个
+a.append('one')
+# extend添加可迭代对象
+a.extend(['4','5'])
+a[len(a):] = ['6']
+# print(a[])
 # 1、List 写在方括号之间，元素用逗号隔开。
 # 2、和字符串一样，List 可以被索引和切片。
 # 3、List 可以使用 + 操作符进行拼接。
