f1a51b5b94fc0c33af3122c9fc053b0d66b90d4f
liufei <liufei@zhuanzhuan.com>
2022-09-20 00:22:43 +0800

    2022 09 20 36

--- learn/list2.py
+++ learn/list2.py
@@ -15,3 +15,22 @@ print(x)
 x = [1,2,3,4,5]
 del x[:]
 print(x)
+# 判断序列每个是否为真
+print(all([1,0,1]))
+# 返回List 元组和索引
+print(list(enumerate((1,2,3),10)))
+# 返回list 矩阵
+print(list(zip([1,2,3],[4,5])))
+print(list(zip([1,2,3],'li')))
+import itertools
+from sys import intern
+from timeit import repeat
+# 不会丢掉短的序列
+print(list(itertools.zip_longest([1,2,3],'li')))
+print(list(map(pow,[1,2,3],[2,4,1])))
+print(list(filter(str.islower,'lIawa')))
+y = iter(list('123'))
+print(next(y))
+print(next(y))
+print(next(y))
+print(next(y,'没了'))
